745b036e3b0e8c9193ed4cb52377d893b2f42db5
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-12-01 11:50:44 +0100

    add and remove img if selected in product

--- src/js/script.js
+++ src/js/script.js
@@ -56,7 +56,7 @@
       this.id = id;
       this.data = data;
       this.renderInMenu();
-      this.getElement();
+      this.getElements();
       this.initAccordion();
       this.initOrderForm();
       this.proccesOrder();
@@ -70,12 +70,13 @@
       menuContainer.appendChild(this.element);
     }
 
-    getElement() {
+    getElements() {
       this.accordionTrigger = this.element.querySelector(select.menuProduct.clickable);
       this.form = this.element.querySelector(select.menuProduct.form);
       this.formInputs = this.form.querySelectorAll(select.all.formInputs);
       this.cartButton = this.element.querySelector(select.menuProduct.cartButton);
       this.priceElem = this.element.querySelector(select.menuProduct.priceElem);
+      this.imageWrapper = this.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -109,12 +110,7 @@
     }
 
     proccesOrder() {
-      const thisProduct = this;
-
-
-      const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
-
+      const formData = utils.serializeFormToObject(this.form);
       let price = this.data.price;
 
       for (let paramId in this.data.params) {
@@ -122,8 +118,18 @@
 
         for (let optionId in param.options) {
           const option = param.options[optionId];
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
 
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionImage = this.imageWrapper.querySelector(`.${paramId}-${optionId}`);
+          if (optionImage) {
+            if (optionSelected) {
+              optionImage.classList.add('active');
+            } else {
+              optionImage.classList.remove('active');
+            }
+          }
+
+          if (optionSelected) {
             if (!option.default) {
               price += option.price;
             }
@@ -134,38 +140,25 @@
           }
         }
       }
-
       this.priceElem.innerHTML = price;
-
-      console.log(price);
     }
   }
 
   const app = {
     initMenu: function () {
-      const thisApp = this;
 
-      for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+      for (let productData in this.data.products) {
+        new Product(productData, this.data.products[productData]);
       }
     },
 
     initData: function () {
-      const thisApp = this;
-
-      thisApp.data = dataSource;
+      this.data = dataSource;
     },
 
     init: function () {
-      const thisApp = this;
-      // console.log('*** App starting ***');
-      // console.log('thisApp:', thisApp);
-      // console.log('classNames:', classNames);
-      // console.log('settings:', settings);
-      // console.log('templates:', templates);
-
-      thisApp.initData();
-      thisApp.initMenu();
+      this.initData();
+      this.initMenu();
     },
   };
 
