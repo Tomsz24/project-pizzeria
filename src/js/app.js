4fa22dbff40a6fa0f3b04ca430bb4ea0d13fa27d
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-12-22 21:45:27 +0100

    modify settings.js and app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,46 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    this.pages = document.querySelector(select.containerOf.pages).children;
+    this.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = this.pages[0].id;
+
+    for (let page of this.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    this.activatePage(pageMatchingHash);
+
+    for (let link of this.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+
+        window.location.hash = `#/${id}`;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    for (let page of this.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for (let link of this.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == `#${pageId}`);
+    }
+
+  },
   initMenu: function () {
     for (let productData in this.data.products) {
       new Product(this.data.products[productData].id, this.data.products[productData]);
@@ -35,6 +73,7 @@ const app = {
   init: function () {
     this.initData();
     this.initCart();
+    this.initPages();
   },
 };
 
